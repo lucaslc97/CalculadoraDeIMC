5f7c4512c3046ea5f1b61341890030b841124f6c
lucaslc97 <49406931+lucaslc97@users.noreply.github.com>
2020-01-13 17:39:24 -0200

    80%
    80% pronto. Está faltando o calculo de peso ideal.

--- scripts/index.js
+++ scripts/index.js
@@ -39,6 +39,7 @@ function calcularImcHomem()
 
     var peso = parseFloat(document.getElementById("peso").value)
     var altura = parseFloat(document.getElementById("altura").value)
+    
 
     console.log("PESO "+peso)
     console.log("ALTURA "+altura)
@@ -53,36 +54,43 @@ function calcularImcHomem()
 
         if (imc < 17 )
         {
-            document.getElementById('res').innerHTML = `Seu IMC é de: ${imc.toFixed(2)} <br><br>Você esta muito abaixo do peso`
+            document.getElementById('alerta').innerHTML = "<strong> ALERTA!!!! <strong>"
+            document.getElementById('res').innerHTML = `Seu IMC é de: ${imc.toFixed(2)}. <br>Você esta muito abaixo do peso.`
+            document.getElementById('img').style.background = "red"
+
+            
         } else if ( imc >= 17 && imc <= 18.49)
         {
-            document.getElementById('res').innerHTML = `Seu IMC é de: ${imc.toFixed(2)} <br><br>Você está com o peso abaixo do normal`
+            document.getElementById('alerta').innerHTML = "<strong> ALERTA!!!! <strong>"
+            document.getElementById('img').style.background = "red"
+            document.getElementById('res').innerHTML = `Seu IMC é de: ${imc.toFixed(2)} . <br>Você está com o peso abaixo do normal.`
         } else if (imc >= 18 && imc <= 24.99)
         {
-            document.getElementById('res').innerHTML = `Seu IMC é de: ${imc.toFixed(2)} <br><br>Você está com o peso normal`
+            document.getElementById('res').innerHTML = `Seu IMC é de: ${imc.toFixed(2)} . <br>Você está com o peso normal.`
         }
         else if (imc >= 25 && imc <= 29.99)
         {
-            document.getElementById('res').innerHTML = `Seu IMC é de: ${imc.toFixed(2)} <br><br>Você está acima do peso`
+            document.getElementById('alerta').innerHTML = "<strong> ALERTA!!!! <strong>"
+            document.getElementById('img').style.background = "red"
+            document.getElementById('res').innerHTML = `Seu IMC é de: ${imc.toFixed(2)} . <br>Você está acima do peso.`
         }
         else if (imc >= 30 && imc <= 34.99)
         {
-            document.getElementById('res').innerHTML = `Seu IMC é de: ${imc.toFixed(2)} <br><br>Você está com obesidade nivel I `
+            document.getElementById('alerta').innerHTML = "<strong> ALERTA!!!! <strong>"
+            document.getElementById('img').style.background = "red"
+            document.getElementById('res').innerHTML = `Seu IMC é de: ${imc.toFixed(2)} . <br>Você está com obesidade nivel I. `
         }
         else 
         {
-            document.getElementById('res').innerHTML = `Seu IMC é de: ${imc.toFixed(2)} <br><br>Você está com obesidade nivel II (Severa)`
+            document.getElementById('alerta').innerHTML = "<strong> ALERTA!!!! <strong>"
+            document.getElementById('img').style.background = "red"
+            document.getElementById('res').innerHTML = `Seu IMC é de: ${imc.toFixed(2)} . <br><br>Você está com obesidade nivel II (Severa)`
         }
     }
     
-    
-
-
-
-
-
 
 }
+
 
 
 
